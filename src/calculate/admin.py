ad877b8f351c910fcd7bc822f45dee5044f12e7f
Harvey Allen <57222485+harvey-allen@users.noreply.github.com>
2023-10-22 19:33:38 +0100

    parameter admin

--- src/calculate/admin.py
+++ src/calculate/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from calculate.models import CreditParameters
 
-# Register your models here.
+
+class CreditParametersAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'occupation', 'payment_behaviour', 'number_of_loans', 'annual_income', 'credit_score')
+    search_fields = ('name', 'occupation')
+    ordering = ('-credit_score',)
+
+
+admin.site.register(CreditParameters, CreditParametersAdmin)
