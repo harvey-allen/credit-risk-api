41f38dd7b98292a38be658af2ce198fdb62f9b00
Harvey Allen <57222485+harvey-allen@users.noreply.github.com>
2023-11-01 16:29:50 +0000

    documentation

--- src/calculate/api/viewsets.py
+++ src/calculate/api/viewsets.py
@@ -9,6 +9,21 @@ from calculate.models import CreditParameters
 
 
 class CreditParametersViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for managing credit parameters data.
+
+    This ViewSet provides CRUD (Create, Retrieve, Update, Delete) operations for CreditParameters objects.
+    It uses the CreditParameters model and the CreditParametersSerializer for serialization.
+
+    Methods:
+        - perform_create(serializer): Creates a new CreditParameters object. It predicts the credit score
+          based on the provided data using a pre-trained model and saves the prediction to the object.
+
+    Attributes:
+        - log: A logger for recording events related to credit parameters.
+        - queryset: The set of CreditParameters objects to be retrieved and manipulated.
+        - serializer_class: The serializer class to be used for data serialization.
+    """
 
     log = logging.getLogger('credit_parameters')
 
