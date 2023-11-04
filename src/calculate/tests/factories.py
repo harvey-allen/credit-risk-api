8479fb352ea1ec6ace5e4e7f19e56c87c3cfd17e
Harvey Allen <57222485+harvey-allen@users.noreply.github.com>
2023-11-04 21:32:31 +0000

    credit parameter factory

---
+++ src/calculate/tests/factories.py
@@ -0,0 +1,12 @@
+import factory
+from calculate.models import CreditParameters
+
+
+class CreditParametersFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CreditParameters
+
+    id = factory.Faker('uuid4')
+    name = factory.Faker('name')
+    month = factory.Faker('date_this_month')
+    occupation = factory.Faker('job')
